98b110c76eb6bdd2e4f917772e7d36ab9a681deb
cynthia <cynthiaawsajira@gmail.com>
2025-01-12 16:54:41 +0300

    add js for the pomodoro project

---
+++ pomodoro/pomodoro.js
@@ -0,0 +1,19 @@
+const isRunning = false;
+const workTime = 25;
+const breakTime = 5;
+const isWorkTime = true;
+let timeLeft;
+
+const minutesDisplay = document.getElementById('minutes');
+const secondsDisplay = document.getElementById('seconds');
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const resetButton = document.getElementById('reset');
+const workTimeInput = document.getElementById('work-time');
+const breakTimeInput = document.getElementById('break-time');
+const statusDisplay = document.getElementById('status');
+const progressBar = document.getElementById('progress');
+
+
+
+
